7720d0d1f97b9cdbe8937f367d1ab24342d67c2e
Muhammad Akhta <akhtacaesar@gmail.com>
2023-10-14 00:37:12 +0700

    Revert

--- src/ReduxSaga/Reducer/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux'
-import RegionReducer from './RegionReducer'
-
-const rootReducer = combineReducers({
-    regionState: RegionReducer
-})
-
-export default rootReducer
